3f75e24000737f47bc5d07ecc6e4f686956ae9ba
brarkhushpreet <127877083+brarkhushpreet@users.noreply.github.com>
2026-09-23 15:27:52 +0530

    Add: two-integer-sum - submission-12

---
+++ Data Structures & Algorithms/two-integer-sum/submission-12.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int [][] arr= new int[nums.length][2];
+        for(int i=0;i<nums.length;i++){
+            arr[i][0]=nums[i];
+            arr[i][1]=i;
+        }
+        Arrays.sort(arr,(a, b) -> Integer.compare(a[0], b[0]));
+        int i=0;
+        int j=nums.length-1;
+
+        while(i<j){
+        int curr=arr[i][0]+arr[j][0];
+                if(curr==target){
+                    return new int[]{Math.min(arr[i][1],arr[j][1]),Math.max(arr[i][1],arr[j][1])};
+                }else if(curr>target){
+                    j--;
+                }else{
+                    i++;
+                }
+                 
+            
+        }
+        return new int[]{};
+    }
+}
